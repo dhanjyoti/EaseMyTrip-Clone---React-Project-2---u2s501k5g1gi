1fbc8c0b1ca48e0b54f019c8fc924fc5362dd8aa
dhanjyoti <dhanjyotir20@gmail.com>
2023-11-10 20:33:43 +0530

    navbar updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,20 @@ import './componentStyles/header.css'
 import Logo from '../images/emtlogo_new6';
 import NavItems from './NavItems';
 
-const Header = ({text}) => {
+const Header = () => {
   return (
     <div className='main-div'>
         <div>
             <div><Logo /></div>
-            <ul>
+            <ul className='nav-item-list'>
                 <li><NavItems text={"FLIGHT"} /></li>
+                <li><NavItems text={"HOTELS"} /></li>
+                <li><NavItems text={"FLIGHT + HOTEL"} /></li>
+                <li><NavItems text={"TRAINS"} /></li>
+                <li><NavItems text={"BUS"} /></li>
+                <li><NavItems text={"HOLIDAYS"} /></li>
+                <li><NavItems text={"CABS"} /></li>
+                <li><NavItems text={"ACTIVITIES"} /></li>
             </ul>
         </div>
         <div></div>

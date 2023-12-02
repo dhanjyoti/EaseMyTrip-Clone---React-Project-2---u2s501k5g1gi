c2a741b99488ad996945b74f663a920da5344a98
dhanjyoti <dhanjyotir20@gmail.com>
2023-12-02 23:03:16 +0530

    dialog component initiated

---
+++ src/components/dialog/Dialog.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import * as DialogP from '@radix-ui/react-dialog';
+
+const Dialog = ({ title, content, trigger }) => (
+    <DialogP.Root>
+        <DialogP.Trigger asChild>
+            {trigger}
+        </DialogP.Trigger>
+        <DialogP.Portal>
+            <DialogP.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
+            <DialogP.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+                <DialogP.Title className="text-mauve12 m-0 text-[17px] font-medium">
+                    {title}
+                </DialogP.Title>
+                {content}
+                {/* <div className="mt-[25px] flex justify-end">
+          <DialogP.Close asChild>
+            <button className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
+              Save changes
+            </button>
+          </DialogP.Close>
+        </div>
+        <DialogP.Close asChild>
+          <button
+            className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+            aria-label="Close"
+          >
+            <Cross2Icon />
+          </button>
+        </DialogP.Close> */}
+            </DialogP.Content>
+        </DialogP.Portal>
+    </DialogP.Root>
+);
+
+export default Dialog;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,31 +5,32 @@ import NavItems from './NavItems';
 import JoinEmtpro from '../../images/emtpro-header-icon';
 import HelpComponent from './HelpComponent';
 import DownArrow from '../../images/down-arrow-5-svgrepo-com';
+import Dialog from '../dialog/Dialog';
 
 const navList = [
   {
-    label:"FLIGHT"
+    label: "FLIGHT"
   },
   {
-    label:"HOTELS"
+    label: "HOTELS"
   },
   {
-    label:"FLIGHT + HOTEL"
+    label: "FLIGHT + HOTEL"
   },
   {
-    label:"TRAINS"
+    label: "TRAINS"
   },
   {
-    label:"BUS"
+    label: "BUS"
   },
   {
-    label:"HOLIDAYS"
+    label: "HOLIDAYS"
   },
   {
-    label:"CABS"
+    label: "CABS"
   },
   {
-    label:"ACTIVITIES"
+    label: "ACTIVITIES"
   },
 
 ]
@@ -41,7 +42,7 @@ const Header = () => {
       <div className='flex flex-row'>
         <div><Logo /></div>
         <ul className='flex flex-row items-center list-none'>
-          {navList.map((item)=><li key={item.label} className='p-[10px] border-r border-gray-500'><NavItems text={item.label} /></li>)}
+          {navList.map((item) => <li key={item.label} className='p-[10px] border-r border-gray-500'><NavItems text={item.label} /></li>)}
 
           <li className='p-[10px]'><NavItems text={"More"} /></li>
         </ul>
@@ -56,7 +57,26 @@ const Header = () => {
           </div>
           <div>
             <HelpComponent className={"flag"} text={"India"} icon={<DownArrow />} />
-            <HelpComponent className={"profile"} text={"My Account"} icon={<DownArrow />} />
+            <Dialog
+              title={"Login or Create an account"}
+              content={
+                <div>
+                  <div>
+                    Email ID or Mobile Number
+                  </div>
+                  <div>
+                    <input type='text' placeholder='Enter email ID' />
+                  </div>
+                  <div>
+                    {/* <input type='button'>CONTINUE</input> */}
+                  </div>
+                  <div>
+                    {/* By logging in, I understand & agree to EaseMyTrip
+                    <span>terms of use</span> and <span>privacy policy</span> */}
+                  </div>
+                </div>
+              }
+              trigger={<button><HelpComponent className={"profile"} text={"My Account"} icon={<DownArrow />} /></button>} />
           </div>
         </div>
       </div>

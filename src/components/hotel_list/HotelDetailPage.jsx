cff54915c0ccaefd8b5b530613b59a27566cf5ae
dhanjyoti <dhanjyotir20@gmail.com>
2024-01-02 15:59:46 +0530

    hotel details page wip

--- src/components/hotel_list/HotelDetailPage.jsx
+++ src/components/hotel_list/HotelDetailPage.jsx
@@ -1,31 +1,69 @@
-import React from 'react'
-import BookingPolicy from './BookingPolicy'
+import React from "react";
+import BookingPolicy from "./BookingPolicy";
+import StarRating from "../../images/hotelComponent/icon-star.svg";
+import MapMaker from "../../images/hotelComponent/map-marker.svg";
 
 const HotelDetailPage = () => {
   return (
-    <div className='my-44 '>
-        <div>
-            <div>
-                <div></div>
-                <div>
-                    ``
-                </div>
+    <div className="my-44 ">
+      <div>
+        <div className="flex flex-row justify-between p-[10px] pb-[2px] rounded-[5px] shadow-[0_0_7px_0_rgb(0_0_0_/_25%)]">
+          <div>
+            <div className="flex flex-row items-baseline">
+              <span className="text-[21px] font-semibold">
+                Zion A Luxurious Hotel Bangalore
+              </span>
+              <span className="flex flex-row">
+                <img src={StarRating} />
+                <img src={StarRating} />
+                <img src={StarRating} />
+                <img src={StarRating} />
+                <img src={StarRating} />
+              </span>
+              <span className="border border-[#3C9AEE] text-xs py-[1px] px-[10px] font-normal rounded-[4px]">
+                Hotel
+              </span>
             </div>
-            <div>
-
+            <div className="flex flex-row text-[#9F9C9C] text-[13px] font-medium my-[6px]">
+              <img src={MapMaker} />
+              <div>
+                31 W.H. Hanumanthappa Road 5Th Main Off Race Course Road
+              </div>
             </div>
+          </div>
+          <div className="flex flex-row items-center">
+            <div className="pr-2 text-right">
+              <div className="text-[13px] font-semibold">Very Good</div>
+              <div className="text-[#555555] text-xs">25 reviews</div>
+            </div>
+            <div className="bg-[#00A664] rounded-[10px] text-[13px] font-semibold p-[6px] text-white inline-block">5.0</div>
+          </div>
         </div>
-        <div className='py-[10px] px-[15px] rounded-[5px] border border-[#d5d5d5] shadow-sm'>
-            <span className='py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold'>Room</span>
-            <span className='py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold'>Room</span>
-            <span className='py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold'>Room</span>
-            <span className='py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold'>Room</span>
-            <span className='py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold'>Room</span>
-            <span className='py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold'>Room</span>
-        </div>
-        <BookingPolicy/>
+        <div></div>
+      </div>
+      <div className="py-[10px] px-[15px] rounded-[5px] border border-[#d5d5d5] shadow-sm">
+        <span className="py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold">
+          Room
+        </span>
+        <span className="py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold">
+          Room
+        </span>
+        <span className="py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold">
+          Room
+        </span>
+        <span className="py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold">
+          Room
+        </span>
+        <span className="py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold">
+          Room
+        </span>
+        <span className="py-[10px] px-[30px] hover:text-blue-600 border-b-4 hover:border-blue-600 text-sm font-semibold">
+          Room
+        </span>
+      </div>
+      <BookingPolicy />
     </div>
-  )
-}
+  );
+};
 
-export default HotelDetailPage
+export default HotelDetailPage;

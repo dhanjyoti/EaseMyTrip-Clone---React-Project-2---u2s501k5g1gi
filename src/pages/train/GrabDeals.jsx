67dd2ea2f55bd704c3229d148d40c0726a371476
Dhanjyoti Rabha <dhanjyotirabha@Dhanjyotis-MacBook-Air.local>
2024-01-08 22:18:14 +0530

    WIP

--- src/pages/train/GrabDeals.jsx
+++ src/pages/train/GrabDeals.jsx
@@ -1,109 +1,110 @@
 import React from 'react'
 
 const GrabDeals = () => {
-  return (
-    <div className='bg-[#ececec] py-[50px] px-[155px] text-[13px]'>
-        <h3 className='text-center'>Grab Best Deals on Train Tickets</h3>
-        <div>
-            <p>
-            The railway is the most affordable way to travel in India, and as a trustworthy travel brand, EaseMyTrip understands the value of your money and desire to travel. To make your train ticket booking more convenient, we are pleased to become IRCTC Authorized partner. Customer can book their train tickets in the most convenient way with our website. You just need to visit our website or download our mobile app to make your train ticket booking with us.
-            <br/>
-            <br/>
-            Travelling across the country has become easy due to online train ticket booking. You can visit almost every city by train at an affordable price. Train ticket booking provides several advantages such as low-cost travel, speed, convenience, and a picturesque journey. If you are planning to go to your hometown, a beautiful destination for your vacation, workcation, or any other place, the train is the best mode of transport for you. At EaseMyTrip, you can find multiple trains for your route, timings, fares, and seats. Also, if you want to save on your railway ticket, download EaseMyTrip and get notifications for all the latest offers on train ticket booking. At EaseMyTrip, we always come up with lots of exciting offers through which you can save money on travel.
-            <br/>
-            <br/>
-            Now, you don’t need to wait for hours standing in queue for ticket booking as EaseMyTrip is offering you an opportunity for train ticket booking online. It is very easy to book train tickets with EaseMyTrip. You need to have your Railway user Id, and you are good to go. Not only this, you will also get to enjoy live station status, live train status, pnr status, 24/7 customer service, train food booking, Instant cancellation and refunds, when you book train tickets through us. Not only this, you also save money by booking through our user-friendly portal. While booking train tickets for your route, you can also apply filter to search and book train ticket instantly. You will get a complete list of trains for your route from which you can choose as per your preference i.e. train type, seat type, category type, quota, etc.
-            <br/>
-            <br/>
-            <span>Documents to Carry</span>
-            <br/>
-            When your railway ticket booking is confirmed with EaseMyTrip, you need to carry your government recognized ID on the departure date. Train ticket-checker may come any time and may ask you to show railway booking along with your ID. Here is the list of IDs that you can carry while boarding the train:
-            <br/>
-            Carry any one of them:
-            <br/>
-            - Aadhar Card
-            <br/>
-            - Voter ID
-            <br/>
-            - Pan card
-            <br/>
-            - Passport
-            <br/>
-            - Driving License
-            <br/>
-            - Bank passbook that was your photograph.
-            <br/>
-            - Student ID card with photograph, issued by recognized school/college
-            <br/>
-            <br/>
-            At EaseMyTrip, you can go for train booking online with different types of quotas in railway such as ladies quota, tatkal booking, general booking, senior citizen booking, etc. So, what are you waiting for? Go for it!
-            <br/>
-            <br/>
-            Best Strategies to Book Tatkal Train Tickets with Ease
-            <br/>
-            In India, it’s very challenging to obtain Tatkal train tickets. Various factors, such as accelerating demand or advanced bookings, are the major reasons demonstrating why getting Tatkal train tickets is not everyone’s cup of tea.
-            <br/>
-            So, if you’re searching for effective ways to book it, then here are some effective mantras via which you can book Tatkal Train Tickets at the last minute. Now, let’s preview these valuable pointers extensively.
-            <br/>
-            <br/>
-            • Be Prepared in Advance
-            <br/>
-            It’s highly advised to prepare all the information related to names, age, and identity credentials in advance.
-            <br/>
-            <br/>
-            • Rely on Better Network Connections
-            <br/>
-            It’s better to have fast, efficient, and reliable internet networks or connections to eliminate all the delays flawlessly while making train bookings.
-            <br/>
-            <br/>
-            • Find the Best Payment Option
-            <br/>
-            From various payment gateways, you must stick to only one payment method that ensures fast transactions without any delay.
-            <br/>
-            <br/>
-            • Create a Back-Up Plan
-            <br/>
-            If you want to secure a Tatkal ticket, then having a legitimate backup plan is the best option through which at least you can get an alternate train of a distinct class.
-            <br/>
-            <br/>
-            • Monitor the Seat Availability
-            <br/>
-            Sometimes, it happens that not everyone gets an opportunity to book Tatkal tickets on the spot. During such cases, you can constantly keep a tab on other seats because by chance, someone can cancel their tickets, and you can be the lucky one.
-            <br/>
-            Now, these are the effective strategies that you can use to enhance your chances of booking Tatkal tickets right away successfully. We hope that this information is insightful and we wish you a heartfelt happy journey for your future.
-            <br/>
-            <br/>
-            <br/>
-            How to Book Train Ticket Online
-            <br/>
-            Booking train tickets with EaseMyTrip is extremely easy. Follow the below-mentioned steps to make your train booking with EaseMyTrip:
-            <br/>
-            Step 1: Visit easemytrip.com and then click on Trains tab.
-            <br/>
-            Step 2: Type the name of the departure point in ‘From’ section and destination point in "To" section. Also, select the departure date and then click on Search button.
-            <br/>
-            Step 3: Now, you will see a list of trains according to your search. Choose the train and seat as per your preference. Use filter option to select seat type or category.
-            <br/>
-            Step 4: Click continue button if the departure and destination station are confirmed.
-            <br/>
-            Step 5: Now, you have to mention all the mandatory details related to your booking. Enter the IRCTC user id, select the boarding point, enter contact details and traveller information (the details are to be mentioned separately for every passenger/child).
-            <br/>
-            Step 6: Once all the details are filled on Review &Travellers page, review all the details to avoid any issue. Check the Price Summary and if you are okay with everything, go ahead with your payment.
-            <br/>
-            Step 7: If you have any coupon code, enter the same in the required field and save your money.
-            <br/>
-            Step 8: Now, make your payment using your preferred option. You can choose from a wide range of options from Debit card, Credit card, UPI, net banking, or wallet money.
-            <br/>
-            Step 9: Once the payment is done, IRCTC authentication page will open, and you are required to fill in your IRCTC user ID password after that you will also receive your e-ticket on mobile and email ID.
-            <br/>
-            <br/>
-            This process for booking train tickets online is simpler with EaseMyTrip. It is better to book your travel online rather than standing in queue for long hours. You just need to have IRCTC user id to book your train tickets with us. In case you don’t have IRCTC user id, you can easily create it online on the official site or by visiting our website.
-            <br/>
-            Disclaimer : The train running or station information is not affiliated with or endorsed by Indian Railways or IRCTC.
-            </p>
+    return (
+        <div className='bg-[#ececec] py-[50px] px-[155px] text-[13px] leading-6 font-medium'>
+            <h3 className='text-center text-xl font-semibold'>Grab Best Deals on Train Tickets</h3>
+            <div>
+                <p>
+                    The railway is the most affordable way to travel in India, and as a trustworthy travel brand, EaseMyTrip understands the value of your money and desire to travel. To make your train ticket booking more convenient, we are pleased to become IRCTC Authorized partner. Customer can book their train tickets in the most convenient way with our website. You just need to visit our website or download our mobile app to make your train ticket booking with us.
+                    <br />
+                    <br />
+                    Travelling across the country has become easy due to online train ticket booking. You can visit almost every city by train at an affordable price. Train ticket booking provides several advantages such as low-cost travel, speed, convenience, and a picturesque journey. If you are planning to go to your hometown, a beautiful destination for your vacation, workcation, or any other place, the train is the best mode of transport for you. At EaseMyTrip, you can find multiple trains for your route, timings, fares, and seats. Also, if you want to save on your railway ticket, download EaseMyTrip and get notifications for all the latest offers on train ticket booking. At EaseMyTrip, we always come up with lots of exciting offers through which you can save money on travel.
+                    <br />
+                    <br />
+                    Now, you don’t need to wait for hours standing in queue for ticket booking as EaseMyTrip is offering you an opportunity for train ticket booking online. It is very easy to book train tickets with EaseMyTrip. You need to have your Railway user Id, and you are good to go. Not only this, you will also get to enjoy live station status, live train status, pnr status, 24/7 customer service, train food booking, Instant cancellation and refunds, when you book train tickets through us. Not only this, you also save money by booking through our user-friendly portal. While booking train tickets for your route, you can also apply filter to search and book train ticket instantly. You will get a complete list of trains for your route from which you can choose as per your preference i.e. train type, seat type, category type, quota, etc.
+                    <br />
+                    <br />
+                    <strong>Documents to Carry</strong>
+                    <br />
+                    When your railway ticket booking is confirmed with EaseMyTrip, you need to carry your government recognized ID on the departure date. Train ticket-checker may come any time and may ask you to show railway booking along with your ID. Here is the list of IDs that you can carry while boarding the train:
+                    <br />
+                    Carry any one of them:
+                    <br />
+                    - Aadhar Card
+                    <br />
+                    - Voter ID
+                    <br />
+                    - Pan card
+                    <br />
+                    - Passport
+                    <br />
+                    - Driving License
+                    <br />
+                    - Bank passbook that was your photograph.
+                    <br />
+                    - Student ID card with photograph, issued by recognized school/college
+                    <br />
+                    <br />
+                    At EaseMyTrip, you can go for train booking online with different types of quotas in railway such as ladies quota, tatkal booking, general booking, senior citizen booking, etc. So, what are you waiting for? Go for it!
+                    <br />
+                    <br />
+                    <strong>Best Strategies to Book Tatkal Train Tickets with Ease</strong>
+                    <br />
+                    In India, it’s very challenging to obtain Tatkal train tickets. Various factors, such as accelerating demand or advanced bookings, are the major reasons demonstrating why getting Tatkal train tickets is not everyone’s cup of tea.
+                    <br />
+                    So, if you’re searching for effective ways to book it, then here are some effective mantras via which you can book Tatkal Train Tickets at the last minute. Now, let’s preview these valuable pointers extensively.
+                    <br />
+                    <br />
+                    <strong>• Be Prepared in Advance</strong>
+                    <br />
+                    It’s highly advised to prepare all the information related to names, age, and identity credentials in advance.
+                    <br />
+                    <br />
+                    <strong>• Rely on Better Network Connections</strong>
+                    <br />
+                    It’s better to have fast, efficient, and reliable internet networks or connections to eliminate all the delays flawlessly while making train bookings.
+                    <br />
+                    <br />
+                    <strong>• Find the Best Payment Option</strong>
+                    <br />
+                    From various payment gateways, you must stick to only one payment method that ensures fast transactions without any delay.
+                    <br />
+                    <br />
+                    <strong>• Create a Back-Up Plan</strong>
+                    <br />
+                    If you want to secure a Tatkal ticket, then having a legitimate backup plan is the best option through which at least you can get an alternate train of a distinct class.
+                    <br />
+                    <br />
+                    <strong>• Monitor the Seat Availability</strong>
+                    <br />
+                    Sometimes, it happens that not everyone gets an opportunity to book Tatkal tickets on the spot. During such cases, you can constantly keep a tab on other seats because by chance, someone can cancel their tickets, and you can be the lucky one.
+                    <br />
+                    Now, these are the effective strategies that you can use to enhance your chances of booking Tatkal tickets right away successfully. We hope that this information is insightful and we wish you a heartfelt happy journey for your future.
+                    <br />
+                    <br />
+                    <br />
+                    <strong>How to Book Train Ticket Online</strong>
+                    <br />
+                    Booking train tickets with EaseMyTrip is extremely easy. Follow the below-mentioned steps to make your train booking with EaseMyTrip:
+                    <br />
+                    Step 1: Visit easemytrip.com and then click on Trains tab.
+                    <br />
+                    Step 2: Type the name of the departure point in ‘From’ section and destination point in "To" section. Also, select the departure date and then click on Search button.
+                    <br />
+                    Step 3: Now, you will see a list of trains according to your search. Choose the train and seat as per your preference. Use filter option to select seat type or category.
+                    <br />
+                    Step 4: Click continue button if the departure and destination station are confirmed.
+                    <br />
+                    Step 5: Now, you have to mention all the mandatory details related to your booking. Enter the IRCTC user id, select the boarding point, enter contact details and traveller information (the details are to be mentioned separately for every passenger/child).
+                    <br />
+                    Step 6: Once all the details are filled on Review &Travellers page, review all the details to avoid any issue. Check the Price Summary and if you are okay with everything, go ahead with your payment.
+                    <br />
+                    Step 7: If you have any coupon code, enter the same in the required field and save your money.
+                    <br />
+                    Step 8: Now, make your payment using your preferred option. You can choose from a wide range of options from Debit card, Credit card, UPI, net banking, or wallet money.
+                    <br />
+                    Step 9: Once the payment is done, IRCTC authentication page will open, and you are required to fill in your IRCTC user ID password after that you will also receive your e-ticket on mobile and email ID.
+                    <br />
+                    <br />
+                    This process for booking train tickets online is simpler with EaseMyTrip. It is better to book your travel online rather than standing in queue for long hours. You just need to have IRCTC user id to book your train tickets with us. In case you don’t have IRCTC user id, you can easily create it online on the official site or by visiting our website.
+                    <br />
+                    <br />
+                    <strong>Disclaimer:</strong> The train running or station information is not affiliated with or endorsed by Indian Railways or IRCTC.
+                </p>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default GrabDeals

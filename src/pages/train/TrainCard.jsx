e827d0cd86bd27d843716e92f768cd5bbe38443c
Dhanjyoti Rabha <dhanjyotirabha@Dhanjyotis-MacBook-Air.local>
2024-02-12 22:42:31 +0530

    wip

--- src/pages/train/TrainCard.jsx
+++ src/pages/train/TrainCard.jsx
@@ -18,7 +18,7 @@ const TrainCard = () => {
         <div className='flex flex-row items-center justify-between p-[14px]'>
             <div>
                 <div className='text-base font-semibold'>Netaji Express</div>
-                <div className='bg-[#F2F9FF] border border-[#B6D5F0] text-[15px] font-medium px-[5px] py-[3px] rounded-[3px]'>12134</div>
+                <div className='bg-[#F2F9FF] border border-[#B6D5F0] text-[15px] font-medium px-[5px] py-[3px] rounded-[3px] inline-block'>12134</div>
             </div>
             <div>
                 <div className='text-base font-semibold'>04:44</div>
@@ -41,11 +41,11 @@ const TrainCard = () => {
             </div>
             <div className='w-36'></div>
         </div>
-        <div className='border-t border-[#eee]'>
+        <div className='border-t border-[#eee] p-[14px]'>
             <div className='flex flex-row justify-between items-center'>
-                <div>Seat Availability</div>
-                <div className='flex flex-row items-center'>
-                    <div>Quota</div>
+                <div className='text-[14px] font-medium'>Seat Availability</div>
+                <div className='flex flex-row items-center gap-2'>
+                    <div className='text-[14px] font-medium'>Quota</div>
                     <div>
                         <select className="border border-[#cccccc] rounded-full px-2 py-2 text-[13px]" name="quotaSelectdd0" id="quotaSelectdd0" quota="">
                             <option ng-repeat="_Quota in Quota" value="GN">General</option>
@@ -58,7 +58,18 @@ const TrainCard = () => {
                 </div>
             </div>
             <div>
-
+                <div className='inline-block py-2'>
+                    <div className='border border-[#f9f2b4] bg-[#fefae5] flex flex-col items-center rounded-[4px] px-[10px] pt-2 pb-[6px]'>
+                        <div className='text-[11px] font-medium'>Sleeper Class (SL)</div>
+                        <div className='text-[11px] font-semibold'>
+                            <span>₹</span>
+                            <span>678</span>
+                        </div>
+                        <div className='text-red-500 font-semibold text-[11px]'>RLWL150/WL108</div>
+                        <button className='bg-[#ef6614] text-white text-[11px] rounded-full px-[10px] py-[2px] font-semibold'>BOOK NOW</button>
+                    </div>
+                    <div className='text-[10px] text-[#737373] text-center'>yesterday</div>
+                </div>
             </div>
         </div>
     </div>

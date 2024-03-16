617159705625499fc0a4df73423833615636c968
Dhanjyoti Rabha <dhanjyotirabha@Dhanjyotis-MacBook-Air.local>
2024-03-17 00:23:01 +0530

    flight booking initiated

--- src/pages/flight/FlightBooking.jsx
+++ src/pages/flight/FlightBooking.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import api from "../../utils/api";
+import medicalCoverage from "../../images/flightCardImage/coverage-icon-v1.svg";
+import RightTeak from "../../images/flightCardImage/blueticknw.svg";
+import LikeIcon from "../../images/flightCardImage/f-icon-9.png";
+
 const fromPoint = [
   {
     place: "Delhi(DEL)",
@@ -63,7 +67,7 @@ const FlightBooking = () => {
   return (
     flight && (
       <div className="flex flex-row gap-7 px-14 py-5">
-        <div className="flex flex-col flex-1">
+        <div className="flex flex-col gap-6 flex-1">
           <div className="flex flex-col rounded overflow-hidden p-1.5">
             <div
               className="h-[50px] flex flex-row items-center px-3"
@@ -71,6 +75,7 @@ const FlightBooking = () => {
                 background: "linear-gradient(90deg, #c7dffe 0%, #d8f2ff 100%)",
               }}
             >
+              <span></span>
               Flight Detail
             </div>
             <div className="p-5 shadow-md">
@@ -107,9 +112,53 @@ const FlightBooking = () => {
               </div>
             </div>
           </div>
-          <div>hello</div>
-          <div>hello</div>
-          <div>hello</div>
+
+          <div className="shadow-sm shadow-[#b8d0e5] rounded text-xs">
+            <div className="flex flex-row gap-2 px-3 py-3">
+              <img src={medicalCoverage} alt="medical coverage" />
+              <div>
+                <p className="flex flex-row gap-1">
+                  <h3 className="text-[#2196f3] text-[15px] font-semibold">FREE Medical Refund Policy</h3>
+                  <img src={RightTeak}/>
+                </p>
+                <p>Get full airline refund, if you cancel tickets due to illness or sickness. This  service is provided at sickness. This service is provided at <b>ZERO additional charges</b>.</p>
+              </div>
+            </div>
+            <div className="pl-6 bg-[#f7f7f7] py-3">
+              <p className="flex flex-row gap-2 pb-[6px]">
+                <input className="w-[18px]" type="radio" name="select"/>
+                <p>Yes, I want to add Medical Refund Policy (FREE) to this flight</p>
+              </p>
+              <p className="flex flex-row gap-2">
+                <input className="w-[18px]" type="radio" name="select"/>
+                <p>No, I don't wish to add Medical Refund Policy (FREE) to this flight</p>
+              </p>
+            </div>
+          </div>
+
+          <div className="shadow-sm shadow-[#b8d0e5] rounded text-xs">
+              <div className="flex flex-row items-center border-b border-[#c4e4b8] gap-2 bg-[#dcfbd0] pt-[6px] px-6 pb-2">
+                <img className="w-[34px] h-[34px]" src={LikeIcon} />
+                <div>
+                  <h3 className="text-[15px] font-semibold">Good to Know</h3>
+                  <p>Information you should know </p>
+                </div>
+              </div>
+              <ul className="list-disc mx-9 py-2">
+                <li>15 Kgs per passenger Check-in Baggage included for your selected flight on the sector  Delhi to Mumbai</li>
+                <li>Airline Cancellation Fee is Rs 3180 per passenger for your selected flight on the sector Delhi to Mumbai</li>
+                <li>Remember to web check-in before arriving at the airport</li>
+                <li>Face masks are advisable</li>
+              </ul>
+          </div>
+
+          <div className="shadow-sm shadow-[#b8d0e5] rounded bg-[#e3f8fa] text-xs py-[10px] px-6">
+            <h3 className="text-[15px] font-semibold">Important Information</h3>
+            <ul className="list-disc px-3">
+              <li className="pt-2">Wearing face masks is no longer compulsory. However, it’s highly advised to wear masks to stay protected from threats imposed by COVID-19.</li>
+              <li className="pt-1">Travellers can check the detailed travel guidelines issued by the Indian government. <span className="text-[#337ab7]">Click here...</span> to know more.</li>
+            </ul>
+          </div>
           <div>hello</div>
           <div>hello</div>
         </div>

f9e0fc3b796dd293da319477d9f63510550b3fdb
Dhanjyoti Rabha <dhanjyotirabha@Dhanjyotis-MacBook-Air.local>
2024-04-11 12:54:06 +0530

    wip

--- src/components/TopFlightRoutes.js
+++ src/components/TopFlightRoutes.js
@@ -6,7 +6,7 @@ const TopFlightRoutes = ({image, takeOff, landing, routeCode}) => {
     <div className='flex flex-row gap-2'>
         <div><img className='rounded-md' src={image} /></div>
         <div>
-            <div className='flex flex-row items-center'>
+            <div className='flex flex-row items-center gap-2'>
                 <span>{takeOff}</span>
                 <span><IconAirplane/></span>
                 <span>{landing}</span>
